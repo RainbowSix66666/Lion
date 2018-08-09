3148af5aac78babef2208f7e172023b60e098f75
DongJieLam <ldj2472599@163.com>
2018-08-09 10:40:01 +0800

    添加订单的Dao层接口

---
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IOrderDao.java
@@ -0,0 +1,20 @@
+package com.rainbowsix.cbec.dao;
+
+import java.util.List;
+
+import com.rainbowsix.cbec.model.OrderModel;
+
+//订单的DAO层接口
+public interface IOrderDao {
+	//新增订单
+	public void create(OrderModel order) throws Exception;
+	//修改订单
+	public void update(OrderModel order) throws Exception;
+	//删除订单
+	public void delete(OrderModel order) throws Exception;
+	//取得所有订单列表
+	public List<OrderModel> selectOrderListByAll() throws Exception;
+	//通过订单ID取得订单
+	public OrderModel selectOrderListById() throws Exception;
+
+}
