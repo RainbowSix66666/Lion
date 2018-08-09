28c776c1e9cd0a21e3271b0563149b266d10100e
admin <admin@C6502018>
2018-08-09 13:58:49 +0800

    完善RoleMapper

--- CBEC/src/main/java/com/rainbowsix/cbec/dao/IRoleDao.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IRoleDao.java
@@ -7,6 +7,7 @@ public interface IRoleDao {
 	public void delece(RoleModel role) throws Exception;
 	public void update(RoleModel role) throws Exception;
 	public void selectByRoleName(String name) throws Exception;
+	public void selectById(int id) throws Exception;
 	public void selectAll() throws Exception;
 	public void setMenuId(RoleModel role) throws Exception;
 }
--- CBEC/src/main/java/com/rainbowsix/cbec/model/RoleModel.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/model/RoleModel.java
@@ -1,12 +1,26 @@
 package com.rainbowsix.cbec.model;
 
 public class RoleModel {
+	private int id;
 	private String name;
 	private int menuid;
 	private String detial;
+	
 	public String getName() {
 		return name;
 	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
