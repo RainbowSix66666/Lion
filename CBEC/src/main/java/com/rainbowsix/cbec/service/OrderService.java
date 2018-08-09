c15ce09a8358ae7749814250663f215418df4e61
DongjieLam <ldj2472599@163.com>
2018-08-09 14:08:33 +0800

    添加订单业务层接口

---
+++ CBEC/src/main/java/com/rainbowsix/cbec/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.rainbowsix.cbec.service;
+
+import java.util.List;
+
+import com.rainbowsix.cbec.model.OrderModel;
+
+//订单的业务层接口
+public interface OrderService {
+	//新增订单
+	public void add(OrderModel order) throws Exception;
+	//修改订单
+	public void modify(OrderModel order) throws Exception;
+	//删除订单
+	public void delete(int orderid) throws Exception;
+	//取得所有订单列表
+	public List<OrderModel> getOrderListByAll() throws Exception;
+	//通过订单ID取得订单
+	public OrderModel getOrderListById(int orderid) throws Exception;
+
+}
