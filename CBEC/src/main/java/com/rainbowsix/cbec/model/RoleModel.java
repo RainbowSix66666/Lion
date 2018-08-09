db36507f3ad112c1969d0b90dcb138d8cf89ecdb
admin <admin@C6502018>
2018-08-09 11:06:34 +0800

    添加Role

--- CBEC/src/main/java/com/rainbowsix/cbec/model/RoleModel.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/model/RoleModel.java
@@ -4,6 +4,24 @@ public class RoleModel {
 	private String name;
 	private int menuid;
 	private String detial;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMenuid() {
+		return menuid;
+	}
+	public void setMenuid(int menuid) {
+		this.menuid = menuid;
+	}
+	public String getDetial() {
+		return detial;
+	}
+	public void setDetial(String detial) {
+		this.detial = detial;
+	}
 	
 	
 }
