c347a63fee494e0db345cbaa1991aa04da31dfb2
admin <admin@C6502018>
2018-08-09 11:17:19 +0800

    添加Role，User的DAO接口

---
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IRoleDao.java
@@ -0,0 +1,10 @@
+package com.rainbowsix.cbec.dao;
+
+import com.rainbowsix.cbec.model.RoleModel;
+
+public interface IRoleDao {
+	public void create(RoleModel role) throws Exception;
+	public void delece(RoleModel role) throws Exception;
+	public void update(RoleModel role) throws Exception;
+	public void selectByRoleName(String name) throws Exception;
+}
---
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IUserDao.java
@@ -0,0 +1,15 @@
+package com.rainbowsix.cbec.dao;
+
+import java.util.List;
+
+import com.rainbowsix.cbec.model.UserModel;
+
+public interface IUserDao {
+	
+	public void create(UserModel userModel) throws Exception;
+	public void update(UserModel userModel) throws Exception;
+	public void delete(UserModel userModel) throws Exception;
+	public List<UserModel> selectAllWithPage(int rows, int page)
+		throws Exception;
+
+}
