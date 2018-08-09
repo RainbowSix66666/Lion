4f4d1c03ebb1bd5785912accc8db1bdf48439bd5
admin <admin@C6502021>
2018-08-09 11:34:22 +0800

    添加Mapper.xml

--- CBEC/src/main/java/com/rainbowsix/cbec/dao/IOrderDao.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IOrderDao.java
@@ -11,10 +11,10 @@ public interface IOrderDao {
 	//修改订单
 	public void update(OrderModel order) throws Exception;
 	//删除订单
-	public void delete(OrderModel order) throws Exception;
+	public void delete(int orderid) throws Exception;
 	//取得所有订单列表
 	public List<OrderModel> selectOrderListByAll() throws Exception;
 	//通过订单ID取得订单
-	public OrderModel selectOrderListById() throws Exception;
+	public OrderModel selectOrderListById(int orderid) throws Exception;
 
 }
