d3eaa37eb162eac687e3ae148c33e94d23ca87b1
admin <admin@C6502022>
2018-08-09 09:29:59 +0800

    增加管理员账户model

---
+++ CBEC/src/main/java/com/rainbowsix/cbec/model/AdminModel.java
@@ -0,0 +1,30 @@
+package com.rainbowsix.cbec.model;
+
+public class AdminModel {
+	private int Admin_id;
+	private String password;
+	private String account;
+	
+	
+	public int getAdmin_id() {
+		return Admin_id;
+	}
+	public void setAdmin_id(int admin_id) {
+		Admin_id = admin_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	
+	
+	
+}
