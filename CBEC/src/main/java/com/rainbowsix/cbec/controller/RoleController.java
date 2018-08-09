011c41ac1aa464ecde9b88ec76a983000bd92cb8
admin <admin@C6502018>
2018-08-09 14:34:11 +0800

    添加RoleCOntroller的Add方法

--- CBEC/src/main/java/com/rainbowsix/cbec/controller/RoleController.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/controller/RoleController.java
@@ -1,5 +1,32 @@
 package com.rainbowsix.cbec.controller;
 
-public class RoleController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.rainbowsix.cbec.model.RoleModel;
+import com.rainbowsix.cbec.service.IRoleService;
+
+@RestController
+@RequestMapping("/role")
+public class RoleController {
+	private IRoleService roleService = null;
+	
+	@Autowired
+	public void setRoleService(IRoleService roleService) {
+		this.roleService = roleService;
+	}
+	
+	@RequestMapping(value="/add",method={RequestMethod.GET})
+	@ResponseBody
+	public String add(RoleModel role) throws Exception{
+		
+		
+		
+		return "nice";
+	}
+	
+	
 }
