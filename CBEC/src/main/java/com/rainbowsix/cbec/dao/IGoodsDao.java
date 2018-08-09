b379da6ed832467cae7edc56c2250d18631a4f11
admin <admin@C6502019>
2018-08-09 10:33:57 +0800

    完善IGoodsDao

--- CBEC/src/main/java/com/rainbowsix/cbec/dao/IGoodsDao.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IGoodsDao.java
@@ -14,9 +14,14 @@ public interface IGoodsDao {
 	//删除商品
 	public void delete(GoodsModel good) throws Exception;
 	
-	//取得所有商品列表
-	public List<GoodsModel> All() throws Exception;
+	//分页取得所有商品列表
+	public List<GoodsModel> AllByPage(int rows,int page) throws Exception;
 	
 	//取得指定的商品信息
 	public GoodsModel byNO(int no) throws Exception;
+	
+	//取得所有商品总数
+	public int Count() throws Exception;
+	
+	
 }
