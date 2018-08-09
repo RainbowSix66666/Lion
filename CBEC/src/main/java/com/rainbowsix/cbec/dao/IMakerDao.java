2c9971a14d4ed30109606a5f50248848ccffde38
admin <admin@C6502022>
2018-08-09 11:26:08 +0800

    增加makerDao

---
+++ CBEC/src/main/java/com/rainbowsix/cbec/dao/IMakerDao.java
@@ -0,0 +1,19 @@
+package com.rainbowsix.cbec.dao;
+
+import java.util.List;
+
+import com.rainbowsix.cbec.model.MakerModel;
+
+
+
+public interface IMakerDao {
+		//新增品牌制造商
+		public void create(MakerModel maker) throws Exception;
+		//修改订单
+		public void update(MakerModel maker) throws Exception;
+		//删除订单
+		public void delete(MakerModel maker) throws Exception;
+		//通过品牌商id取得品牌商的所有商品
+		public List<MakerModel> selectProductListByMakerID() throws Exception;
+		
+}
