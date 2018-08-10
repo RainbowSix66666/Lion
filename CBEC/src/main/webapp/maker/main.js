f94f5eaa3be4564c3f60ff3711340d54d459745f
www <www@DESKTOP-7LIB0H2>
2018-08-10 23:53:29 +0800

    更新context.xml

--- CBEC/src/main/webapp/maker/main.js
+++ CBEC/src/main/webapp/maker/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-		//页面操作代码区
+		/*//页面操作代码区
 		$("ul#demo1 li a").on("click",function(event){
 			var href = $(this).attr("href");
 			alert(href);
 			$("div#main_body").load(href);
 			event.preventDefault();
-		});
+		});*/
+	//请求取得部门列表的REST API
+	$.getJSON("maker/list/all.mvc", function(makerList){
+		alert(makerList);
+	});
+	alert("ok");
 });
