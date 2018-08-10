d2d23a8aae1364c85f47135135ce00ab88ba31a8
admin <admin@C6502018>
2018-08-10 16:28:07 +0800

    修改页面

---
+++ CBEC/src/main/webapp/index.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+	$("ul#main_menu li ul a").on("click", function(event){
+		var href = $(this).attr("href");
+		href = "/User/main.html";
+		alert(href);
+		$("div#main_body").load(href);
+		
+		event.preventDefault();
+	});
+})
