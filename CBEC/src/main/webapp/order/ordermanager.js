76dec9ad9cc881519ecace717f1475a8c9ff9326
DongJieLam <ldj2472599@163.com>
2018-08-11 16:32:07 +0800

    添加订单管理页面

---
+++ CBEC/src/main/webapp/order/ordermanager.js
@@ -0,0 +1,13 @@
+/**
+ * 订单管理的主JS
+ */
+$(document).ready(function(){
+	//请求取得订单列表的Rest API
+	$.getJSON("order/list/all.mvc",function(orderList){
+		var lines="";
+		for(var i=0;i<orderList.length;i++){
+			lines=lines+"<tr><td>"+orderList[i].orderid+"</td><td>"+orderList[i].comid+"</td><td>"+orderList[i].orderstate+"</td><td>"+orderList[i].bsid+"</td></tr>"
+		}
+		$("table#orderListTable tbody").html(lines);
+	}); 
+});
