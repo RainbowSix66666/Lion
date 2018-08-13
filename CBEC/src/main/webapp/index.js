b1952e8e08451d557a5ea3a07ac842a99264ff24
admin <admin@C6502021>
2018-08-13 10:19:00 +0800

    tii

--- CBEC/src/main/webapp/index.js
+++ CBEC/src/main/webapp/index.js
@@ -2,10 +2,25 @@
 $(document).ready(function(){
 	$("ul#main_menu li ul a").on("click", function(event){
 		var href = $(this).attr("href");
-		href = "/user/main.html";
-		alert(href);
-		$("div#main_body").load(href);
-		
+		$("div#main_body").load(href);		
+		event.preventDefault();
+	});
+	$("div#goods a").on("click", function(event){
+		var href = $(this).attr("href");
+		$("div#main_body").load(href);		
+		event.preventDefault();
+	});
+	//我的订单
+	$("div#order a").on("click", function(event){
+		var href = $(this).attr("href");
+		$("div#main_body").load(href);		
+		event.preventDefault();
+	});
+	
+	//订单管理
+	$("ul#demo1 li a").on("click", function(event){
+		var href = $(this).attr("href");
+		$("div#main_body").load(href);		
 		event.preventDefault();
 	});
 })
