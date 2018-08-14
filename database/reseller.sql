a9dc9fdf4f4b3a333361bacfb0fa5eaefff5296f
Muzhangyong <904489394@qq.com>
2018-08-14 20:50:08 +0800

    添加reseller表的SQL

---
+++ database/reseller.sql
@@ -0,0 +1,8 @@
+create table reseller(
+       reseller_id number primary key,
+       reseller_area varchar(128),
+       reseller_location varchar(128),
+       reseller_mail varchar(64),
+       reseller_phone_pre char(3),
+       reseller_phone_suf char(11)
+);
