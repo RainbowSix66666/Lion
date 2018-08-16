7430d98d0213aac989271d6b0cc1537ecfd86c1e
www <www@DESKTOP-7LIB0H2>
2018-08-17 01:37:41 +0800

    修改photo里js中的属性

--- CBEC/src/main/webapp/goodsphoto/main.js
+++ CBEC/src/main/webapp/goodsphoto/main.js
@@ -7,7 +7,6 @@
 //页面载入成功事件
 
 $(document).ready(function(){
-	alert("1");
 	var photoNo = null;
 	/*显示员工列表表格*/
 	$("table#goodsphotoGrid").jqGrid({
@@ -16,18 +15,18 @@ $(document).ready(function(){
 		 styleUI : 'Bootstrap',
          datatype: "json",
          colModel: [
-             { label: '账号', name: 'photoId', key: true, width: 75 },
-             { label: '姓名', name: 'proid', width: 150 },
-             { label: '性别', name: 'des', width: 150 },
-             { label: '年龄', name: 'age', width: 150 },
-             { label:'生日', name: 'rank', width: 150 },
+             { label: '照片id', name: 'photoId', key: true, width: 75 },
+             { label: '商品id', name: 'good.proid', width: 150 },
+             { label: '照片说明', name: 'des', width: 150 },
+             { label: '商品名称', name: 'good.title', width: 150 },
+             { label:'照片等级', name: 'rank', width: 150 },
          ],
          autowidth:true,
 		 viewrecords: true,
 		 width: "100%",
          height: 400,
          rowNum: 3,
-         rowList: [1,2,3],
+         rowList: [1,2,3,5,10,15],
          pager: "#goodsphotoGridPager",
 		jsonReader : {
 		     root: "rows",  //指定数据列表
