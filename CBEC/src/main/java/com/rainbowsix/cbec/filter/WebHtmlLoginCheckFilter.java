2b8c1d2d6bf91565a7f32281219f5375672348e2
admin <admin@C6502022>
2018-08-30 15:13:47 +0800

    拦截器实现

--- CBEC/src/main/java/com/rainbowsix/cbec/filter/WebHtmlLoginCheckFilter.java
+++ CBEC/src/main/java/com/rainbowsix/cbec/filter/WebHtmlLoginCheckFilter.java
@@ -8,6 +8,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet Filter implementation class WebHtmlLoginCheckFilter
@@ -33,11 +37,43 @@ public class WebHtmlLoginCheckFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		System.out.println("过滤器工作");
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		//取得web站点的名称
+		String webRoot = req.getServletContext().getContextPath();
+		String url = req.getRequestURI();
+		if(url.equals(webRoot+"/login.html")) {
+			chain.doFilter(request, response);
+		}else{
+			HttpSession session = req.getSession();
+			if(session.getAttribute("userInfo") == null) {
+				res.sendRedirect(webRoot+"/login.html");
+			}else {
+				chain.doFilter(request, response);
+			}
+			
+		}
+	
+		/*HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		//取得web站点的名称
+		String webRoot = req.getServletContext().getContextPath();
+		String url = req.getRequestURI();
+		if(url.equals(webRoot+"/login.html")) {
+			chain.doFilter(request, response);
+		}
+		else {
+			HttpSession session = req.getSession();
+			if(session.getAttribute("userInfo")!=null) {
+				
+				chain.doFilter(request, response);
+			}
+			else{
+				res.sendRedirect(webRoot+"/login.html");
+			}
+			
+		}*/
 	}
 
 	/**
